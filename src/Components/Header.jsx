09b691dfdac457ebf4407cf414a1a4af503dbc88
MahAzevedo <madavetto@gmail.com>
2024-10-25 00:15:38 -0300

    Assets: Logo svg

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Header.module.css'
+import { Link } from 'react-router-dom';
+// import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      Header
+      <nav className="container">
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">o svg de cara de cachorro tá quebrado ||| 
+        {/* <Dogs /> */}
+        </Link>
+        <Link className={styles.login} to="/login">        Login / Criar</Link>
+      </nav>
     </div>
   );
 };
