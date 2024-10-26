41d72a37159af670111816ee67e42010d88724a0
MahAzevedo <madavetto@gmail.com>
2024-10-26 00:08:05 -0300

    modified: src/Components/Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className={styles.header}>
-      <nav className="container">
+      <nav className={`styles.logo`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">o svg de cara de cachorro tá quebrado ||| 
         {/* <Dogs /> */}
         </Link>
