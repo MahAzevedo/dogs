8b0653ed707f28de1b01c961f141bbfbbe57da3d
MahAzevedo <madavetto@gmail.com>
2024-10-29 00:11:32 -0300

    modified: src/App.css & modified: src/Components/Header.jsx & modified: src/Components/Header.module.css & modified: src/img/dog.png

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,12 +3,15 @@ import styles from './Header.module.css'
 import { Link } from 'react-router-dom';
 // import { ReactComponent as Dogs } from '../Assets/bird.avif';
 // import Dog from './img/dog.svg?react';
-// import dog from '../Assets/dogs.png?react';
 import dog from '../img/dog.png?react';
 
 const Header = () => {
   return (
     <header className={styles.header}>
+
+      <img className='logoDog' src={dog} alt="Logo cachorro feliz" />  {/* esse img é um teste */}
+
+
       <nav className={`styles.nav container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">o svg de cara de cachorro tá quebrado ||| 
         {/* <Dogs /> */}
