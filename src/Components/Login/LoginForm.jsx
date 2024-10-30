2505949488d6b679af997b0a2f3bc066c6ca1406
MahAzevedo <madavetto@gmail.com>
2024-10-30 01:23:20 -0300

    new file: src/Components/Forms/Button.jsx & Button.module.css & LoginForm.jsx

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginForm = () => {
   const [username, setUsername] = React.useState('');
@@ -27,18 +29,10 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input 
-        type="text" 
-        value={username}
-        onChange={(target) => setUsername(target.value)} 
-        />
-        <input 
-        type="text" 
-        value={password}
-        onChange={(target) => setPassword(target.value)} 
-        />
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
       </form>
-      <button>Entrar</button>
+      <Button>Entrar</Button>
       <Link to="/login/criar">Cadastro</Link>
     </section>
   );
