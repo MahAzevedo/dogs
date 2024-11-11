164b4ff8dd5f13ea064c21884ba919a173bb6437
MahAzevedo <madavetto@gmail.com>
2024-11-11 20:41:27 -0300

    modified: src/Components/Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom';
 // import { ReactComponent as Dogs } from '../Assets/bird.avif';
-// import Dog from './img/dog.svg?react';
+// import Dog from './img/dogs.svg?react';
 // import dog from '../img/dog.png?react';
 import { UserContext } from '../UserContext';
+// import dogFooter from '../img/dogs.footer?react';
+
 
 const Header = () => {
   const {data} = React.useContext(UserContext);
