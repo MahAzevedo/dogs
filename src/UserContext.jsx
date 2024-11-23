de40ec8db099c44668b43b8be9db8a40aa5607d3
MahAzevedo <madavetto@gmail.com>
2024-11-23 02:01:32 -0300

    modified: src/UserContext.jsx & src/api.jsx & src/Components/Header.jsx

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TOKEN_POST } from './api';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
 
 export const UserContext = React.createContext();
 
@@ -9,6 +9,27 @@ export const UserStorage = ({ children }) => {
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(null);
     
+    React.useEffect(() => {
+      async function autoLogin() {
+        const token = window.localStorage.getItem('token');
+        if(token) {
+          try {
+            setError(null);
+            setLoading(true);
+            const { url, options } = TOKEN_VALIDATE_POST('token');
+            const response = await fetch(url, options);
+            if(!response.ok) throw new Error('Token inválido');
+            await getUser(token);
+          } catch (err) {
+            userLogout();
+          } finally {
+            setLoading(false);
+          }
+        }
+      }
+      autoLogin();
+    }, []);
+
     async function getUser(token) {
         const {url, options} = USER_GET(token);
         const response = await fetch(url, options);
@@ -25,8 +46,16 @@ export const UserStorage = ({ children }) => {
         getUser(token);
     }
 
+    async function userLogout() {
+      setData(null);
+      setError(null);
+      setLoading(false);
+      setLogin(false);
+      window.localStorage.removeItem('token');
+    }
+
   return (
-    <UserContext.Provider value={{ userLogin, data }}>
+    <UserContext.Provider value={{ userLogin, data, userLogout }}>
       {children}
     </UserContext.Provider>
   );
--- src/api.jsx
+++ src/api.jsx
@@ -10,8 +10,20 @@ export function TOKEN_POST(body) {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(body), 
-        }
-    }
+        },
+    };
+}
+
+export function TOKEN_VALIDATE_POST(token) {
+    return {
+        url: API_URL + '/jwt-auth/v1/token/validate',
+        options: {
+            method: 'POST',
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        },
+    },
 }
 
 export function USER_GET(token) {
