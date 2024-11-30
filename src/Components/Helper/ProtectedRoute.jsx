b44dcf9482b07feb47dd7213e259ba9c4c7618a0
MahAzevedo <madavetto@gmail.com>
2024-11-29 23:55:07 -0300

    modified: src/Components/Helper/ProtectedRoute.jsx

--- src/Components/Helper/ProtectedRoute.jsx
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -3,9 +3,15 @@ import { UserContext } from '../../UserContext';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({children}) => {
-    const { login } = React.useContext(UserContext);
-
-  return login ? children : <Navigate to="/login" />;
+    
+  const { login } = React.useContext(UserContext);
+    if(login === true) {
+      return children;
+    } else if(login === false) {
+      return <Navigate to="/login" />
+    } else {
+      return <></>
+  }
 };
 
 export default ProtectedRoute;
