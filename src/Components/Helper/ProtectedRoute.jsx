6a4ac4273747b3f0a61a5607ef993146eaf1c320
MahAzevedo <madavetto@gmail.com>
2024-11-30 00:16:15 -0300

    modified: src/Components/Helper/ProtectedRoute.jsx

--- src/Components/Helper/ProtectedRoute.jsx
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import { UserContext } from '../../UserContext';
 import { Navigate } from 'react-router-dom';
 
-// const ProtectedRoute = ({children}) => {
+const ProtectedRoute = ({children}) => {
     
-//   const { login } = React.useContext(UserContext);
-//     if(login === true) {
-//       return children;
-//     } else if(login === false) {
-//       return <Navigate to="/login" />
-//     } else {
-//       return <></>
-//   }
-// };
-
-// export default ProtectedRoute;
-
-
-const ProtectedRoute = ({children}) => { 
   const { login } = React.useContext(UserContext);
-    return login ? children : <Navigate to="/login" />;
+    if(login === true) {
+      return children;
+    } else if(login === false) {
+      return <Navigate to="/login" />
+    } else {
+      return <></>
+  }
 };
 
 export default ProtectedRoute;
+// essa forma não envia para login, primeiro ela confere se já está logado
+
+
+
+// -----------//------
+
+// ou dessa outra forma abaixo: 
+// Primeiro ele mostra a parte de login, depois puxa as informações, vê que o usuário está realmente credenciado, e aí, faz a navegação para /conta
+
+
+// const ProtectedRoute = ({children}) => { 
+//   const { login } = React.useContext(UserContext);
+//     return login ? children : <Navigate to="/login" />;
+// };
+
+// export default ProtectedRoute;
