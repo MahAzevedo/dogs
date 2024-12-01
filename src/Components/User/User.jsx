b5c4d18ac569f6453cfc8a3cfd7b36bf1dcf4b3e
MahAzevedo <madavetto@gmail.com>
2024-12-01 02:14:45 -0300

    new file: src/Components/User/UserHeader.jsx

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import UserHeader from './UserHeader';
+import { Routes, Route } from 'react-router-dom';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from '../User/UserPhotoPost';
+import UserStats from '../User/UserStats';
 
 const User = () => {
   return (
-    <div>
-      Usuário
-    </div>
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
   );
 };
 
 export default User;
+
+// tirei o to do path to, não estava funcionando com ele, acho que houve atualização do React
---
+++ src/Components/User/UserHeaderNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserHeaderNav = () => {
+  return (
+    <nav>
+      Navegação
+    </nav>
+  );
+};
+
+export default UserHeaderNav;
