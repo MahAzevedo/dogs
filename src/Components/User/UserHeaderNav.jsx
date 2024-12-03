da2d3d9e18685e28775d8faddc087924ba8c5b81
MahAzevedo <madavetto@gmail.com>
2024-12-03 19:51:38 -0300

    new file: src/Components/User/UserHeaderNav.module.css

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import MinhasFotos from '../../Assets/feed.svg?react';
+import Estatisticas from '../../Assets/estatisticas.svg?react';
+//import AdicionarFoto from '../../Assets/adicionar.svg?react'; svg quebrado
+import AdicionarFoto from '../../Assets/usuario.svg?react';  //foto adicionar tá quebrada
+import Sair from '../../Assets/sair.svg?react';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
+  // const [mobile, setMobile] = React.useContext(null);
   const { userLogout } = React.useContext(UserContext);
+
   return (
-    <nav>
-      <NavLink to="/conta">Minhas Fotos</NavLink>
-      <NavLink to="/conta/estatisticas">Estatísticas</NavLink>
-      <NavLink to="/conta/postar">Adicionar Foto</NavLink>
-      <button onClick={userLogout}>Sair</button>
+    <nav className={styles.nav}>
+      <NavLink to="/conta">
+        <MinhasFotos /> 
+          {/* {mobile && 'Minhas Fotos'} */}
+        </NavLink>
+      <NavLink to="/conta/estatisticas">
+        <Estatisticas />
+          {/* {mobile && 'Estatísticas'} */}
+        </NavLink>
+
+{/* // coloquei no import a img de usuario, adicionar.svg tá quebrada */}
+      <NavLink to="/conta/postar"> 
+        <AdicionarFoto />
+          {/* {mobile && 'Adicionar Foto'} */}
+      </NavLink>
+
+      <button onClick={userLogout}>  {/* '' -->> userLogout*/}
+        <Sair />
+          {/* {mobile && 'Sair'} */}
+      </button>
     </nav>
   );
 };
