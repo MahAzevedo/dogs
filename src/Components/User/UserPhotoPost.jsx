cf759f3ae56d5a91bbdd1a89424e6e819035d406
MahAzevedo <madavetto@gmail.com>
2024-12-11 23:46:50 -0300

    modified:   src/Components/Home.jsx & src/Hooks/useForm.jsx

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -4,6 +4,9 @@ import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
+import Error from '../Helper/Error';
+import { PHOTO_POST } from '../../api';
+import { useNavigate } from 'react-router-dom';
 
 const UserPhotoPost = () => {
   const nome = useForm();
@@ -11,22 +14,55 @@ const UserPhotoPost = () => {
   const idade = useForm('number');
   const [img, setImg] = React.useState({});
   const {data, error, loading, request} = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (data) navigate('/conta');
+  }, [data, navigate]);
 
   function handleSubmit(event) {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append('img', img.raw);
+    formData.append('nome', nome.value);
+    formData.append('peso', peso.value);
+    formData.append('idade', idade.value);
+
+    const token = window.localStorage.getItem('token');
+    const {url, options} = PHOTO_POST(formData, token);
+    request(url, options);
   }
 
-  function handleImgChange() {}
+  function handleImgChange({target}) {
+    setImg({
+      preview: URL.createObjectURL(target.files[0]),
+      raw: target.files[0],
+    });
+  }
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
       <form onSubmit={handleSubmit}>
-        <Input label="Nome" type="text" name="nome" />
-        <Input label="Peso" type="text" name="peso" />
-        <Input label="Idade" type="text" name="idade" />
-        <input type="file" name="img" id="img" onChange={handleImgChange} />
+        <Input label="Nome" type="text" name="nome" {...nome} />
+        <Input label="Peso" type="number" name="peso" {...peso} /> {/* type -> number*/}
+        <Input label="Idade" type="number" name="idade" {...idade} /> {/* type -> number */}
+        <input className={styles.file} type="file" name="img" id="img" onChange={handleImgChange} 
+      />
+      {loading ? (
+        <Button disebled>Enviando...</Button>
+        ) : (
         <Button>Enviar</Button>
+        )}
+        <Error error={error} />
       </form>
+      <div>
+        {img.preview && (
+          <div 
+          className={styles.preview} 
+          style={{backgroundImage:`url('${img.preview}')`}}>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -4,12 +4,17 @@ const types = {
     email: {
         regex:  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um email válido', 
-    }
-    //,
+    },
+
     // password: {
     //     regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
     //     message: 'A senha precisa ter 1 caracter maiúscula, 1 minúsculo e 1 dígito. Com no mínimo 8 caracteres.'
-    // }
+    // },
+
+    number: {
+        regex: /^\d+$/,
+        message: 'Utilize números apenas.',
+    }
 };
 
 const useForm = (type) => {
