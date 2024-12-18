d224b234c016978fb0a53af4feea5f88f93ad292
MahAzevedo <madavetto@gmail.com>
2024-12-18 16:44:21 -0300

    new file: src/Components/Photo/PhotoComments.jsx

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
 
 const PhotoContent = ({data}) => {
     const {photo, comments} = data;
@@ -14,9 +15,18 @@ const PhotoContent = ({data}) => {
         <div>
             <p>
                 <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+                <span className={styles.visualizacoes}>{photo.acessos}</span>
             </p>
+            <h1 className="title">
+                <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+            </h1>
+            <ul className={styles.attributes}>
+                <li>{photo.peso} kg</li>
+                <li>{photo.idade} anos</li> 
+            </ul>
         </div>
       </div>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
