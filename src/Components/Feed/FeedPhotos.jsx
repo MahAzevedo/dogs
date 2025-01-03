f1c22ba6d1a311487a0b693513d3745a6e73dbd4
MahAzevedo <madavetto@gmail.com>
2025-01-03 16:57:51 -0300

    modified: src/Components/Feed/FeedPhotos.jsx

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -11,8 +11,10 @@ const FeedPhotos = ({page, user, setModalPhoto}) => {
 
   React.useEffect(() => {
     async function fetchPhotos() {
+      const total = 3
       const { url, options } = PHOTOS_GET({ page, total: 3, user });
       const {response, json} = await request(url, options);
+      if(response && response.ok && json.length < total)
     }
     fetchPhotos();
   }, [request, user, page]);
