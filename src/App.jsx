6231f03a4353b4003869bbb07623a56aab3bbe07
MahAzevedo <madavetto@gmail.com>
2025-01-09 20:56:22 -0300

    new file:   src/Components/NotFound.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo/Photo';
-
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
@@ -22,6 +23,8 @@ const App = () => {
             <Route path="login/*" element={<Login />} />
             <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         <Footer />
        </UserStorage>
--- src/Components/User/UserProfile.jsx
+++ src/Components/User/UserProfile.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Feed from '../Feed/Feed';
 
 const UserProfile = () => {
+    const {user} = useParams();
   return (
-    <div>
-      
-    </div>
+    <section className="container mainSection">
+        <h1 className="title">{user}</h1>
+      <Feed user={user} /> 
+    </section>
   );
 };
 
