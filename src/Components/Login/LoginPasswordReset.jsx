6cb5cc9a1f054bb1d3704f009476c71d07536be2
MahAzevedo <madavetto@gmail.com>
2025-01-10 10:59:11 -0300

    modified:   src/Components/Login/LoginPasswordReset.jsx

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
 
 const LoginPasswordReset = () => {
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+    if(key) setKey(key);
+    if(login) setLogin(login);
+  }, [])
+
   return (
     <div>
-      
+      <p>{key}</p>
+      <p>{login}</p>
+      <Input label="Nova Senha" type="password" name="password" /> 
     </div>
-  )
-}
+  );
+};
 
-export default LoginPasswordReset
+export default LoginPasswordReset;
