3b95d56c04f3e8aca449f63c59450afb6e06d8b9
MahAzevedo <madavetto@gmail.com>
2025-01-10 23:25:30 -0300

    new file:   src/Components/Helper/Head.jsx

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -4,13 +4,15 @@ import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
+import { PASSWORD_LOST } from '../../api';
+import Head from '../Helper/Head';
 
 const LoginPasswordLost = () => {
   const login = useForm();
   const {data, loading, error, request} = useFetch();
 
   async function handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefautl();
     if(login.validate()) {
     const {url, options} = PASSWORD_LOST({
       login: login.value, 
@@ -23,12 +25,13 @@ const LoginPasswordLost = () => {
 
   return (
     <section>
+      <Head title="Perdeu a senha" />   
       <h1 className="title">Perdeu a senha?</h1>
       {data ? (
         <p style={{color: '#4c1'}}>{data}</p>
       ) : (
         <form onSubmit={handleSubmit}>
-      <Input label="E-mail / Usuário" type="text" name="email" {...login} />
+      <Input label="E-mail / Usuário" type="text" name="login" {...login} />
       {loading ? (
         <Button disabled>Enviando...</Button>
       ) : (
