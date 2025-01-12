4bf2939de2affe6a66197e694e4f60d247388277
MahAzevedo <madavetto@gmail.com>
2025-01-12 00:04:08 -0300

    modified: src/Components/User/UserStats.jsx   &   src/api.jsx

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import Head from '../Helper/Head';
 import useFetch from '../../Hooks/useFetch';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
 
 const UserStats = () => {
   const {data, error, loading, request} = useFetch();
 
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  React.useEffect(() => {
+    async function getData() {
+      const {url, options} = STATS_GET();
+      await request(url, options)
+    }
+    getData();
+  }, [request]);
+
+  if(loading) return <Loading />
+  if(error) return <Error error={error} /> 
+  if(data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        Estatísticas
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
