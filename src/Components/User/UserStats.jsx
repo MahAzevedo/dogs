01c4643ed1f71c236c7d57660b935b4309bbf74c
MahAzevedo <madavetto@gmail.com>
2025-01-12 21:47:08 -0300

    modified:   package-lock.json  &  modified:   package.json  &  modified:   src/Components/User/UserStats.jsx  -->> to statistics

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
 
 const UserStats = () => {
+  const {data, error, loading, request} = useFetch();
+
   return (
     <div>
       <Head title="Estatísticas" />
