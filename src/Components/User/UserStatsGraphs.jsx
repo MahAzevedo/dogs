efedf8d90cbf74526af803af971488677f294bf0
MahAzevedo <madavetto@gmail.com>
2025-01-17 20:20:34 -0300

    modified:   src/Components/User/UserStatsGraphs.jsx

--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -5,6 +5,10 @@ const UserStatsGraphs = ({data}) => {
     const [graph, setGraph] = React.useState([]);
     const [total, setTotal] = React.useState(0);
 
+    React.useEffect(() => {
+      console.log(data);
+    }, [data]);
+    
   return (
     <section className={`${styles.graph} animeLeft`}>
       <div className={styles.total}>
