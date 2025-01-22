f892a39972b5d09de0b08217792bb5598124a0fe
MahAzevedo <madavetto@gmail.com>
2025-01-22 19:03:05 -0300

    modified:   src/Components/User/UserStats.jsx

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from '../Helper/Head';
 import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
 import Loading from '../Helper/Loading';
 import Error from '../Helper/Error';
 import UserStatsGraphs from './UserStatsGraphs';
@@ -10,11 +11,11 @@ const UserStats = () => {
 
   React.useEffect(() => {
     async function getData() {
-      const {url, options} = STATS_GET();
+      const {url, options} = STATS_GET(); // 
       await request(url, options)
     }
     getData();
-  }, [request]);
+  }, [request]); 
 
   if(loading) return <Loading />;
   if(error) return <Error error={error} />;
--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -21,13 +21,31 @@ const UserStatsGraphs = ({data}) => {
     
   return (
     <section className={`${styles.graph} animeLeft`}>
-      <div className={styles.total}>
+      <div className={`${styles.total} ${styles.graphItem}`}>
         <p>Acessos: {total}</p>
       </div>
-      <div>
+      <div className={styles.graphItem}>
         <VictoryPie 
           data={graph} 
+          innerRadius={50} 
+          padding={{ top: 20, bottom: 20, left: 80, right:80 }} 
+          style={{
+            data: {
+              fillOpacity: .9,
+              stroke: 2,
+              strokeWidth: 2,
+            },
+            labels: {
+              fontSize: 14,
+              fill: '#333',
+            }
+          }}
         />
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryChart>
+          <VictoryBar alignment="start" data={graph}></VictoryBar>
+        </VictoryChart>
       </div>
     </section>
   );
